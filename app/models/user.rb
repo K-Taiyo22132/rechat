85f29aa65be804176e82bf6ac1ae7be4b8587df1
3401 <isha22121@ict.shimanet.ed.jp>
2024-10-11 12:39:27 +0900

    scaffold修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
-  VALID_EMAIL_REGEX = /\AA[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true
   validates :email, length: { maximum: 255 }
   validates :email, format: { with: VALID_EMAIL_REGEX }
--- config/routes.rb
+++ config/routes.rb
@@ -8,19 +8,19 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'profile'
   delete '/users/:id', to: 'users#destroy', as: 'unsubscribe'
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
   get '/image_of_categories/:id', to: 'categories#send_image', as: 'image_of_categories'
 
-=======
+# =======
   get'/image_of_groups/:id',to: 'groups#send_image', as: 'image_of_groups'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-<<<<<<< HEAD
+# <<<<<<< HEAD
   
-=======
->>>>>>> 6d2642ec21d6cd726337e05ef6d45fc049298ce9
->>>>>>> b14de08cbb4880e9fa2ec4bbed57ac09f08f9d62
+# =======
+# >>>>>>> 6d2642ec21d6cd726337e05ef6d45fc049298ce9
+# >>>>>>> b14de08cbb4880e9fa2ec4bbed57ac09f08f9d62
 end
   

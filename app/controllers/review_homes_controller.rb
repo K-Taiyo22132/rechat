41bbe45b3bfc7a102c68f8ade5db97f01d4dcce5
3401 <isha22121@ict.shimanet.ed.jp>
2024-11-15 11:04:00 +0900

    scaffold

--- app/controllers/review_homes_controller.rb
+++ app/controllers/review_homes_controller.rb
@@ -58,6 +58,11 @@ class ReviewHomesController < ApplicationController
     end
   end
 
+  def send_image
+    category = Category.find(params[:id])
+    send_data(category.category_image, disposition: :inline)
+  end
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review_home

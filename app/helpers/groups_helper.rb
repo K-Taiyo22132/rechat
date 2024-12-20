1c795494e01e46b2c05a2411a7da3ac7c0b3714f
3412 <3412@example.com>
2024-12-20 12:17:23 +0900

    12/20

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -9,14 +9,4 @@ module GroupsHelper
   end
 
 end
-  def
-  <%= form_tag(book_search_path) do %>
-    <p>書籍名（部分一致検索）</p>
-    <%= text_field_tag "search[title]", session[:search_title] %><%= submit_tag "検索" %>
-  <% end %>
-  # def
-  # <%= form_tag(book_search_path) do %>
-  #   <p>書籍名（部分一致検索）</p>
-  #   <%= text_field_tag "search[title]", session[:search_title] %><%= submit_tag "検索" %>
-  # <% end %>
-end
+

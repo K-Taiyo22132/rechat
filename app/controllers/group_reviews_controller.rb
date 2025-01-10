cf603db37913448c150cf3c27a7fa248a4901c09
3401 <isha22121@ict.shimanet.ed.jp>
2025-01-10 12:26:24 +0900

    scaffold

--- app/controllers/group_reviews_controller.rb
+++ app/controllers/group_reviews_controller.rb
@@ -4,8 +4,13 @@ class GroupReviewsController < ApplicationController
   # GET /group_reviews or /group_reviews.json
   def index
     @group_reviews = GroupReview.all
+
     #session["selected_group_id_#{current_user.id}"] = params[:group_id]
     @select_categories = SelectCategory.where(user_id: current_user.id)
+    
+      @goods_review = GoodsReview.where("group_id=?",session["selected_group_id_#{current_user.id}"]).order(created_at: :desc)
+      @store_review = StoreReview.where("group_id=?",session["selected_group_id_#{current_user.id}"]).order(created_at: :desc)
+      
   end
 
   # GET /group_reviews/1 or /group_reviews/1.json

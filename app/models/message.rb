1acfe7da223550502edf61f84faad5b5d8ba010e
3412 <3412@example.com>
2025-01-10 12:26:22 +0900

    1/10

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :group,optional: false
   belongs_to :user,optional: false
-  has_many :chats, dependent: :restrict_with_error
+  belongs_to :chat,optional: false
   after_create_commit { MessageBroadcastJob.perform_later self }
 end

9cb638d6fadf3c029f992f204c03ee37e591285e
3401 <isha22121@ict.shimanet.ed.jp>
2025-01-21 14:07:10 +0900

    scaffold

--- app/controllers/group_reviews_controller.rb
+++ app/controllers/group_reviews_controller.rb
@@ -3,6 +3,8 @@ class GroupReviewsController < ApplicationController
 
   # GET /group_reviews or /group_reviews.json
   def index
+    
+    
     @group_reviews = GroupReview.all
     # @group = GroupReview.find(Chat.find(params[:chat_id].group_id))
 
@@ -15,6 +17,14 @@ class GroupReviewsController < ApplicationController
       if params[:selectedcategory].present? 
         @goods_review = @goods_review.where("category_id=?",params[:selectedcategory]).order(created_at: :desc)
         @store_review = @store_review.where("category_id=?",params[:selectedcategory]).order(created_at: :desc)
+      end
+      if params[:search].present? && params[:search][:keyword].present?
+        @goods_review = GoodsReview.where("group_id=? and title like '%#{params[:search][:keyword]}%'", session["selected_group_id_#{current_user.id}"]).order(created_at: :desc)
+        @store_review = StoreReview.where("group_id=? and title like '%#{params[:search][:keyword]}%'",session["selected_group_id_#{current_user.id}"]).order(created_at: :desc)
+      
+      else
+        @goods_review = GoodsReview.where("group_id=?",session["selected_group_id_#{current_user.id}"]).order(created_at: :desc)
+        @store_review = StoreReview.where("group_id=?",session["selected_group_id_#{current_user.id}"]).order(created_at: :desc)
       end
   end
 

8fe5ad95589144b30b0107a53413ad58f4ea8222
3412 <3412@example.com>
2025-01-24 12:23:06 +0900

    1/24

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,22 @@
 module MessagesHelper
+  def customize(message)
+    result = []
+    count= message.size
+    counter = 0
+    counter2 = 16
+    if count >= 16
+    while count >= 16
+      result << message.slice(counter,counter2)
+      result << "\n"
+      counter = counter + 16
+      count = count - 16
+    end
+  else
+    result << message
+  end
+    return result
+
+
+  end
+
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :group,optional: false
   belongs_to :user,optional: false
-  belongs_to :chat,optional: false
+  # belongs_to :chat,optional: false
   after_create_commit { MessageBroadcastJob.perform_later self }
 end
